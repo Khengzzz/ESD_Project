1698e8948495aedaea73241d79f658c288fbf1b9
amanda-ong <amanda.ong.2022@scis.smu.edu.sg>
2024-03-10 18:37:20 +0800

    booking sql updated

--- SQL/bookings.sql
+++ SQL/bookings.sql
@@ -15,25 +15,29 @@ DROP TABLE IF EXISTS `booking`;
 CREATE TABLE IF NOT EXISTS `booking` (
   `booking_id` INT AUTO_INCREMENT PRIMARY KEY,
   `user_id` INT NOT NULL,
-  `event_id` INT NOT NULL,
+  `screening_id` INT NOT NULL,
   `seat_id` JSON NOT NULL,
+  `quantity` INT NOT NULL,
   `booking_status` ENUM('Pending', 'Confirmed', 'Refunded') NOT NULL DEFAULT 'Pending',
   `payment_transaction_id` VARCHAR(255),
-  `payment_status` ENUM('Succeeded', 'Pending', 'Failed'),
   `refund_transaction_id` VARCHAR(255),
   `creation_timestamp` DATETIME DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `booking` (`user_id`, `event_id`, `seat_id`, `booking_status`, `payment_transaction_id`, `payment_status`, `refund_transaction_id`)
+INSERT INTO `booking` (`user_id`, `screening_id`, `seat_id`, `quantity`, `booking_status`, `payment_transaction_id`, `refund_transaction_id`)
 VALUES
-(1, 1, '{"row": 10, "seat": "A"}', 'Confirmed', 'ABC123', 'succeeded', NULL),
-(2, 3, '{"row": 5, "seat": "C"}', 'Pending', NULL, NULL, NULL),
-(3, 7, '{"row": 2, "seat": "D"}', 'Pending', NULL, NULL, NULL),
-(1, 2, '{"row": 15, "seat": "B"}', 'Refunded', 'DEF456', 'failed', 'REF789'),
-(4, 6, '{"row": 8, "seat": "F"}', 'Confirmed', 'GHI789', 'succeeded', NULL),
-(2, 4, '{"row": 3, "seat": "A"}', 'Confirmed', 'JKL012', 'succeeded', NULL),
-(3, 5, '{"row": 12, "seat": "E"}', 'Confirmed', 'MNO345', 'succeeded', NULL),
-(5, 8, '{"row": 20, "seat": "G"}', 'Pending', NULL, NULL, NULL);
+(1, 1, '{"seats": [1,2]}', 2, 'Confirmed', 'ABC123', NULL),
+(2, 3, '{"seats": [3,4,5]}', 3, 'Pending', NULL, NULL),
+(3, 7, '{"seats": [6,7,8,9]}', 4, 'Pending', NULL, NULL),
+(1, 2, '{"seats": [10,11,12]}', 3, 'Refunded', 'DEF456', 'REF789'),
+(4, 6, '{"seats": [13,14,15]}', 3, 'Confirmed', 'GHI789', NULL),
+(2, 4, '{"seats": [16,17]}', 2, 'Confirmed', 'JKL012', NULL),
+(3, 15, '{"seats": [40,41]}', 2, 'Confirmed', 'MNO345', NULL),
+(5, 16, '{"seats": [42,43]}', 2, 'Pending', NULL, NULL),
+(1, 17, '{"seats": [44,45,46,47]}', 4, 'Confirmed', 'ABC123', NULL),
+(2, 18, '{"seats": [48]}', 1, 'Pending', NULL, NULL),
+(3, 19, '{"seats": [49,50]}', 2, 'Pending', NULL, NULL);
+
 
 COMMIT;
 
