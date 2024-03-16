4d33d531e445f18bb3fdba81e592db933df6a834
amanda-ong <amanda.ong.2022@scis.smu.edu.sg>
2024-03-17 01:06:18 +0800

    email added

--- SQL/bookings.sql
+++ SQL/bookings.sql
@@ -15,6 +15,7 @@ DROP TABLE IF EXISTS `booking`;
 CREATE TABLE IF NOT EXISTS `booking` (
   `booking_id` INT AUTO_INCREMENT PRIMARY KEY,
   `user_id` INT NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
   `screening_id` INT NOT NULL,
   `seat_id` JSON NOT NULL,
   `quantity` INT NOT NULL,
@@ -24,19 +25,19 @@ CREATE TABLE IF NOT EXISTS `booking` (
   `creation_timestamp` DATETIME DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `booking` (`user_id`, `screening_id`, `seat_id`, `quantity`, `booking_status`, `payment_transaction_id`, `refund_transaction_id`)
+INSERT INTO `booking` (`user_id`, `email`, `screening_id`, `seat_id`, `quantity`, `booking_status`, `payment_transaction_id`, `refund_transaction_id`)
 VALUES
-(1, 1, '{"seats": [1,2]}', 2, 'Pending', NULL, NULL),
-(2, 2, '{"seats": [3,4,5]}', 3, 'Pending', NULL, NULL),
-(3, 3, '{"seats": [1,2]}', 4, 'Pending', NULL, NULL),
-(1, 1, '{"seats": [1,2,3]}', 3, 'Pending', NULL, NULL),
-(4, 2, '{"seats": [1,2,3]}', 3, 'Pending', NULL, NULL),
-(2, 3, '{"seats": [4,5]}', 2, 'Pending', NULL, NULL),
-(3, 1, '{"seats": [1,5]}', 2, 'Pending', NULL, NULL),
-(5, 2, '{"seats": [3,4]}', 2, 'Pending', NULL, NULL),
-(1, 3, '{"seats": [2]}', 4, 'Pending', NULL, NULL),
-(2, 1, '{"seats": [4]}', 1, 'Pending', NULL, NULL),
-(3, 2, '{"seats": [5]}', 2, 'Pending', NULL, NULL);
+(1, 'user1@example.com', 1, '{"seats": [1,2]}', 2, 'Confirmed', 'PY156', NULL),
+(2, 'user2@example.com', 2, '{"seats": [3,4,5]}', 3, 'Refunded', 'PY839', 'REF712'),
+(3, 'user3@example.com', 3, '{"seats": [1,2]}', 2, 'Confirmed', 'PY3478', NULL),
+(1, 'user1@example.com', 1, '{"seats": [1,2,3]}', 3, 'Pending', NULL, NULL),
+(4, 'user4@example.com', 2, '{"seats": [1,2,3]}', 3, 'Pending', NULL, NULL),
+(2, 'user2@example.com', 3, '{"seats": [4,5]}', 2, 'Pending', NULL, NULL),
+(3, 'user3@example.com', 1, '{"seats": [1,5]}', 2, 'Pending', NULL, NULL),
+(5, 'user5@example.com', 2, '{"seats": [3,4]}', 2, 'Confirmed', 'PY934', NULL),
+(1, 'user1@example.com', 3, '{"seats": [2]}', 1, 'Refunded', 'PY837', 'REF5690'),
+(2, 'user2@example.com', 1, '{"seats": [4]}', 1, 'Pending', NULL, NULL),
+(3, 'user3@example.com', 2, '{"seats": [5]}', 1, 'Pending', NULL, NULL);
 
 
 COMMIT;
