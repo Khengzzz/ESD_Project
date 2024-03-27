0c09722b0039c1e960913dc3a85e1e20007731ce
Khengzzz <khsiew.2022@scis.smu.edu.sg>
2024-03-27 12:11:24 +0800

    some updates

--- backend/app.py
+++ backend/app.py
@@ -19,5 +19,9 @@ def screening():
 def mypurchase():
     return render_template('my_purchase.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=1234, debug=False)
